18a02e4e263b70145776da7d8521d1d64fcf32fb
Kohei Miura <58386600+harple17@users.noreply.github.com>
2020-05-11 07:14:17 +0900

    Update abc_167.c
    add description comments 
    note: study bit full search

--- c/abc_167.c
+++ c/abc_167.c
@@ -9,23 +9,23 @@ int main() {
       for(j = 0; j < m; j++)
       scanf("%d",&a[i][j]);
     }
-    for(i = 0; i < (1 << n); i++) {
-        for(j = 0; j < n; j++)
-        b[j] = 0;
+    for(i = 0; i < (1 << n); i++) { /*full search by shift operation*/
+        for(j = 0; j < m; j++)
+        b[j] = 0; /*b means the sum of improvement on the row condition*/
         for(j = s = 0; j < n; j++) {
             if((i & (1 << j)) == 0)
-            continue;
+            continue; /*not count the "j" condition*/
             s += c[j];
             for(k = 0; k < m; k++)
             b[k] += a[j][k];
         }
         for(j = 0; j < m; j++) {
-            if(b[j] < x)
+            if(b[j] < x) /*if any of skill levels is less than x*/
             goto NEXT;
         }
         if(ans > s)
-        ans=s;
-        NEXT:;
+        ans=s; /*record minimum*/
+        NEXT:; /*not update*/
     }
     printf("%d\n",ans>1e9?-1:ans);
     return 0;
