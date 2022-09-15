1acc9c16c352fe98ea114bb9f07890f8d2567968
harple17 <harpyeagle1706@gmail.com>
2022-09-16 08:19:20 +0900

    update

---
+++ 229/c.cpp
@@ -0,0 +1,49 @@
+//include libraries
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <valarray>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+using namespace std;
+#define rep(i, n) for (int i=0; i < (n); i++)
+#define rep1(i, n, w) for (int i=0; i < (n); i+=j)
+#define rep2(i, s, e, w) for (int i=s; i < (e); i += w)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+    int n, w;
+    cin >> n >> w;
+    vector<int> a(n), b(n);
+    rep(i, n) cin >> a[i] >> b[i];
+
+    
+    return 0;
+}
